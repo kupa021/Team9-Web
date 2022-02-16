012ddce52de777e28a3c48ab484c627677af17d8
meep <arcturus1001@protonmail.com>
2022-02-16 12:40:26 +0200

    javascriptdateandtimeupdate

--- javascript/jscript.php
+++ javascript/jscript.php
@@ -1,3 +1,16 @@
-<h2>JavaScript Date<h2>
+<html>
+<body>
+<script>
+
+<script src="jsdate.js"></script>
+
+<h2>Date and Time<h2>
 <h3>Click this button to access the current time and date<h3>
-    <button onclick="this.innerHTML = date()">Date and Time </button>
+<button onclick ='getElementByID("time").innerHTML = Date()'> Date & Time </button>
+<p id = "time"></p>
+<hr>
+<button onclick = "this.innerHTML = Date()">Date and Time - changes the original content </button>
+
+</script>
+</body>
+</html>
